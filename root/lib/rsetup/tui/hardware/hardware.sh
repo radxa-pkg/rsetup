fd2b8470280b00f26f3f78241e003a5a7c59bd2e
cheanz <1418457284@qq.com>
2022-07-08 14:55:52 +0800

    first edition of hardware menu

--- root/lib/rsetup/tui/hardware/hardware.sh
+++ root/lib/rsetup/tui/hardware/hardware.sh
@@ -2,6 +2,12 @@
 
 __main_hardware() {
     menu_init
+    menu_add __main_about "HDMI"
+    menu_add __main_about "UARTs"
+    menu_add __main_about "SPI"
+    menu_add __main_about "I2Cs"
+    menu_add __main_about "System"
+    menu_add __main_about "Network"
     menu_add __main_about "About"
     menu_show "Configure Radxa product"
 }
