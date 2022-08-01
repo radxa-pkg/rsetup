366814c16a8d587056a9f772204e02a780832472
Yuntian Zhang <yt@radxa.com>
2022-08-01 20:20:46 +0800

    Enable new __on_boot from cmdline

--- root/lib/rsetup/cli/main.sh
+++ root/lib/rsetup/cli/main.sh
@@ -1,3 +1,6 @@
 source "$ROOT_PATH/lib/rsetup/mod/utils.sh"
 source "$ROOT_PATH/lib/rsetup/mod/block_helpers.sh"
 
+source "$ROOT_PATH/lib/rsetup/cli/rconfig.sh"
+source "$ROOT_PATH/lib/rsetup/cli/ssh.sh"
+source "$ROOT_PATH/lib/rsetup/cli/system.sh"
