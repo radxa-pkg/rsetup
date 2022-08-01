218f51209a24c8375d78a77cda3251eb081ffdf2
Yuntian Zhang <yt@radxa.com>
2022-08-01 20:20:20 +0800

    Add regenerate_machine_id

--- root/lib/rsetup/cli/system.sh
+++ root/lib/rsetup/cli/system.sh
@@ -1,3 +1,5 @@
+ALLOWED_RCONFIG_FUNC+=("regenerate_machine_id")
+
 update_bootloader() {
     __parameter_count_check 1 "$@"
     __assert_f "$ROOT_PATH/usr/lib/u-boot-$1/setup.sh"
@@ -7,3 +9,10 @@ update_bootloader() {
     "$ROOT_PATH/usr/lib/u-boot-$1/setup.sh" update_bootloader $device
 }
 
+regenerate_machine_id() {
+    echo "Remove existing machine ids..."
+    rm -f "$ROOT_PATH/etc/machine-id" "$ROOT_PATH/var/lib/dbus/machine-id"
+    echo "Regenerating machine ids..."
+    dbus-uuidgen --ensure="$ROOT_PATH/etc/machine-id"
+    dbus-uuidgen --ensure
+}
