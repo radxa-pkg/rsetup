3fd0ac52df4c8afc5bc293f12e74daab91c8636e
Yuntian Zhang <yt@radxa.com>
2022-08-02 20:56:35 +0800

    Add user_append_group

--- root/lib/rsetup/cli/account.sh
+++ root/lib/rsetup/cli/account.sh
@@ -1,4 +1,4 @@
-ALLOWED_RCONFIG_FUNC+=("add_user" "update_password")
+ALLOWED_RCONFIG_FUNC+=("add_user" "update_password" "user_append_group")
 
 add_user() {
     local username="$1"
@@ -21,4 +21,13 @@ update_password() {
     local password="$2"
     
     echo "$username:$password" | chpasswd
+}
+
+user_append_group() {
+    __parameter_count_check 2 "$@"
+    
+    local username="$1"
+    local group="$2"
+    
+    usermod -aG "$group" "$username"
 }
