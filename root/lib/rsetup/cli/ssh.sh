8f41eed6e48675d50260c6b7b3b62e018354bcde
Tan Guiyang <tan@radxa.com>
2022-08-16 18:11:19 +0800

    SSH installation and uninstallation and enable and disable.

--- root/lib/rsetup/cli/ssh.sh
+++ root/lib/rsetup/cli/ssh.sh
@@ -6,3 +6,31 @@ regenerate_ssh_hostkey() {
     echo "Regenerating SSH host keys..."
     dpkg-reconfigure -f noninteractive openssh-server
 }
+
+install_ssh() {
+    __parameter_count_check 0 "$@"
+    apt-get install openssh-server
+}
+
+uninstall_ssh() {
+    __parameter_count_check 0 "$@"
+    apt-get remove openssh-server   
+}
+
+enable_ssh() {
+    __parameter_count_check 0 "$@"
+    if [ -e /var/log/regen_ssh_keys.log ] && ! grep -q "^finished" /var/log/regen_ssh_keys.log
+    then
+        msgbox "Initial ssh key generation still running. Please wait and try again."
+        return 1
+    fi
+    ssh-keygen -A
+    update-rc.d ssh enable    
+    invoke-rc.d ssh start
+}
+
+disable_ssh() {
+    __parameter_count_check 0 "$@"
+    update-rc.d ssh disable 
+    invoke-rc.d ssh stop 
+}
--- root/lib/rsetup/tui/task/ssh/ssh.sh
+++ root/lib/rsetup/tui/task/ssh/ssh.sh
@@ -1,14 +1,77 @@
-__task_ssh_install(){
-    msgbox "SSH: Install/Uninstall now." 
+__task_ssh_uninstall() {
+    local item
+    item=$(yesno "Are you sure to uninstall SSH service?")
+    if [[ $? == 0 ]]
+    then
+        uninstall_ssh
+        msgbox "Uninstall SSH success."
+    fi     
 }
 
-__task_ssh_enable(){
-    msgbox "SSH: Enable/Disable now." 
+__task_ssh_install() {   
+    local item
+    item=$(yesno "Are you sure to install SSH service?")
+    if [[ $? == 0 ]]
+    then
+        install_ssh
+        item=$(ssh localhost)
+        if [[ $? == 0 ]]
+        then
+            msgbox "Install SSH success."
+        else
+            msgbox "Install SSH failure."
+        fi
+    fi    
 }
 
-__task_ssh(){
+__task_ssh_enable() {
+    local item
+    item=$(yesno "Would you like the SSH server to be enabled?\nCaution: Default and weak passwords are a security risk when SSH is enabled!")
+    if [[ $? == 0 ]]
+    then
+        enable_ssh  
+        if [[ $? == 0 ]]
+        then
+            msgbox "Enable SSH success."
+        else
+            msgbox "Enable SSH failure."    
+        fi 
+    fi    
+}
+
+__task_ssh_disable() {
+    local item
+    item=$(yesno "Would you like the SSH server to be disabled?")
+    if [[ $? == 0 ]]
+    then
+        disable_ssh
+        if [[ $? == 0 ]]
+        then
+            msgbox "Disable SSH success."
+        else
+            msgbox "Disable SSH failure."    
+        fi 
+    fi    
+}
+
+__task_ssh() {
     menu_init
-    menu_add __task_ssh_install   "Install/Uninstall now"
-    menu_add __task_ssh_enable    "Enable/Disable now"
+    local status            
+    status=$(systemctl status ssh  | awk 'NR==1 {print $4}')
+    if [[ "$status" == "OpenBSD" ]]
+    then
+        menu_add __task_ssh_uninstall   "Uninstall SSH"
+    else
+        menu_add __task_ssh_install     "Install SSH"
+    fi
+
+    status=$(systemctl status ssh | grep Active | awk '{print $3}')
+    if [[ "$status" == "(running)" ]]
+    then
+        menu_add __task_ssh_disable     "Disable SSH"
+    elif [[ "$status" == "(dead)" ]]
+    then
+        menu_add __task_ssh_enable      "Enable SSH"
+    fi       
     menu_show "Please select an option below:"
 }
