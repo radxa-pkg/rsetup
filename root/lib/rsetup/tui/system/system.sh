40da3a6fc0e4426913d42972b5c43d4418845e19
cheanz <1418457284@qq.com>
2022-08-17 10:13:03 +0800

    system Chean realized update_bootloader and system_update.
    Signed-off-by: cheanz<zhangchenge@radxa.com>

--- root/lib/rsetup/tui/system/system.sh
+++ root/lib/rsetup/tui/system/system.sh
@@ -1,26 +1,12 @@
 __system_system_update() { 
-    menu_init
-    menu_add __tui_about "About"
-    menu_show "Update System"
-
-    # sudo apt update && sudo apt full-upgrade
+    sudo apt update && sudo apt full-upgrade
 }
 
 __system_update_bootloader() {
-    menu_init
-    menu_add __tui_about "About"
-    menu_show "Update Bootloader"
-
-    # # get curent board name
-    # dtname=output(uname -r)
-    # #get root dev
-    # arr=($(stat / |grep Device))
-    # root_dev=${arr[1]}
-
-    # # dtname-board name mapping
-
-    # # upload boot loader
-    # "$ROOT_PATH/lib/rsetup/cli/main.sh" update_bootloader
+    local cur_board_name="$(dtname)"
+    local root_dev="$(__get_root_dev)"
+    local mapped_board_name="$(echo "$cur_board_name" | tr , -)"
+    "$ROOT_PATH/lib/rsetup/cli/system.sh" update_bootloader $mapped_board_name
 }
 
 __system() {
