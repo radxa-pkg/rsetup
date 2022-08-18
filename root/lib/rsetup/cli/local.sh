bfd29bb52c00bdf018e173e9656d67ebe7e3c05e
PiKaQiuJiEr <tan@radxa.com>
2022-08-18 15:39:10 +0800

    Locale,Keyboard Layout,Display Language,WiFi country,Install CJKV Fonts.

---
+++ root/lib/rsetup/cli/local.sh
@@ -0,0 +1,67 @@
+list_wlan_interfaces() {
+    for dir in /sys/class/net/*/wireless
+    do
+        if [[ -d "$dir" ]]
+        then
+            basename "$(dirname "$dir")"
+        fi
+    done
+}
+
+wifi_country_set() {
+    __parameter_count_check 0 "$@"
+    local iface=$(list_wlan_interfaces | head -n 1)
+    if [[ -z "$iface" ]]
+    then
+        msgbox "No wireless interface found." 
+        return 1
+    fi
+    if [[ ! `wpa_cli -i "$iface" status > /dev/null 2>&1` ]]
+    then
+        msgbox "Could not communicate with wpa_supplicant."
+        return 1
+    fi
+    radiolist_init
+    local status="OFF"
+    local count=$(awk 'END{print NR}' /usr/share/zoneinfo/iso3166.tab)
+    count=$((count-25))
+    for ((i=1;i<=$count;i++))
+    do
+        local country[$i-1]=$(cat /usr/share/zoneinfo/iso3166.tab | tail -n +26 | awk 'NR=='$i' {print $0}' | tr '\t' "  ")
+        radiolist_add "${country[$i-1]}" "$status"   
+    done   
+    radiolist_show "Radiolist"
+    if [[ $? == 0 ]]
+    then
+        local item
+        item=$(yesno "You selected is ${country[${RSETUP_RADIOLIST_STATE_NEW[*]}]}.")
+        if [[ $? == 0 ]]
+        then
+            country=${country[${RSETUP_RADIOLIST_STATE_NEW[*]}]} 
+            country=`echo $country | cut -c 1-2`
+            wpa_cli -i "$iface" set country "$country"
+            wpa_cli -i "$iface" save_config > /dev/null 2>&1
+            iw reg set $country
+            local file="/etc/default/crda"
+            if [[ ! -f $file ]]
+            then
+                touch $file   
+            fi
+
+            local string="REGDOMAIN=$country"
+            local keywords=$(cat $file | grep "REGDOMAIN=")
+
+            if [[ ! -z $keywords ]]
+            then
+                local row=$(grep -n ^REGDOMAIN $file  | cut -d ":" -f 1)
+                sed -i ''$row'd' $file    
+            fi
+            sed -i '$a'$string'' $file     
+            if hash rfkill 2> /dev/null
+            then
+                rfkill unblock wifi
+            fi 
+            msgbox "Wireless LAN country set to $country."
+        fi
+    fi   
+}
--- root/lib/rsetup/tui/local/local.sh
+++ root/lib/rsetup/tui/local/local.sh
@@ -1,29 +1,39 @@
 source "$ROOT_PATH/lib/rsetup/tui/local/timezone/timezone.sh"
-source "$ROOT_PATH/lib/rsetup/tui/local/locale/locale.sh"
-source "$ROOT_PATH/lib/rsetup/tui/local/keyboard_layout/keyboard.sh"
-source "$ROOT_PATH/lib/rsetup/tui/local/display_language/language.sh"
-source "$ROOT_PATH/lib/rsetup/tui/local/wifi_country/wifi_country.sh"
-source "$ROOT_PATH/lib/rsetup/tui/local/install_CJKV_fonts/fonts.sh"
 
-__local_display_language(){
-    msgbox "Display language." 
+__local_locale() {
+    dpkg-reconfigure locales
 }
 
-__local_install_CJKV_fonts(){
-    msgbox "Install CJKV fonts."    
+__local_display_language() {
+    local lg=$(locale | sed -n '1p' | cut -d '=' -f 2)
+    msgbox "Current language used by the system: $lg"     
 }
 
-__local_keyboard_layout(){
+__local_install_CJKV_fonts() {
     local item
-    item=$(yesno "Do you want to modify the keyboard layout?")
+    item=$(yesno "Are you sure to install CJKV fonts?")
     if [[ $? == 0 ]]
     then
-        msgbox "modify..."
-    fi
+        apt-get install fonts-arphic-ukai fonts-arphic-uming fonts-ipafont-mincho fonts-ipafont-gothic fonts-unfonts-core
+        if [[ $? == 0 ]]
+        then
+            msgbox "Install CJKV fonts success."
+        else
+            msgbox "Install CJKV fonts failure."    
+        fi 
+    fi    
 }
 
-__local_wifi_country(){
-    msgbox "WiFi country."    
+__local_keyboard_layout() {
+    dpkg-reconfigure keyboard-configuration
+}
+
+__local_wifi_country() {
+    wifi_country_set
+    if [[ $? != 0 ]]
+    then
+        msgbox "There was an error running option WiFi country." 
+    fi  
 }
 
 __local() {
