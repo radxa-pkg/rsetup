b5af32b4fe3892ebbc504f08b550e2a0de1488ee
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2022-09-16 17:41:03 +0800

    Enforce sane shell behavior

--- usr/lib/rsetup/mod/utils.sh
+++ usr/lib/rsetup/mod/utils.sh
@@ -8,19 +8,15 @@ readonly ERROR_REQUIRE_FILE=-3
 readonly ERROR_ILLEGAL_PARAMETERS=-4
 
 __require_parameter_check() {
-    set -e
-
     if (( $# == 0 ))
     then
         echo "Incorrect usage of ${FUNCNAME[1]} from ${FUNCNAME[2]}: ${FUNCNAME[1]} requires parameter" >&2
         return $ERROR_REQUIRE_PARAMETER
     fi
-    set +e
 }
 
 __parameter_count_check() {
     __require_parameter_check "$@"
-    set -e
 
     local expected=$1
     shift 1
@@ -29,24 +25,20 @@ __parameter_count_check() {
         echo "'${FUNCNAME[1]}' expects '$expected' parameters while getting $#: '$*'" >&2
         return $ERROR_TOO_FEW_PARAMETERS
     fi
-    set +e
 }
 
 __assert_f() {
     __parameter_count_check 1 "$@"
-    set -e
 
     if [[ ! -e "$1" ]]
     then
         echo "'${FUNCNAME[1]}' requires file '$1' to work!" >&2
         return $ERROR_REQUIRE_FILE
     fi
-    set +e
 }
 
 __parameter_value_check() {
     __require_parameter_check "$@"
-    set -e
 
     local option=$1
     shift 1
@@ -55,19 +47,16 @@ __parameter_value_check() {
         echo "'${FUNCNAME[1]}' expects one of '${options[*]}', got '$option'" >&2
         return $ERROR_ILLEGAL_PARAMETERS
     fi
-    set +e
 }
 
 __parameter_type_check() {
     __parameter_count_check 2 "$@"
-    set -e
 
     if [[ $(type -t "$1") != "$2" ]]
     then
         echo "'${FUNCNAME[1]}' expects '$1' type to be '$2', but it is '$(type -t "$1")'." >&2
         return $ERROR_ILLEGAL_PARAMETERS
     fi
-    set +e
 }
 
 __in_array() {
