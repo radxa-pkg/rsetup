4c0e0f015204aa35ec72987ca1652ef36089d7c7
ZHANG Yuntian <yt@radxa.com>
2023-03-11 18:40:13 +0800

    Add kernel hooks for updating overlay

---
+++ src/usr/lib/rsetup/cli/kernel.sh
@@ -0,0 +1,20 @@
+# shellcheck shell=bash
+
+__zz-update-overlays() {
+    # shellcheck disable=SC2034
+    local ABI="${1:-}" IMAGE="${2:-}"
+
+    exec </dev/null >&2
+    eval set -- "$DEB_MAINT_PARAMS"
+    # shellcheck disable=SC2034
+    local ACTION="${1:-}" VERSION="${2:-}"
+
+    case $ACTION in
+        configure)
+            echo "Updating overlays for $ABI ..."
+            reset_overlays "$ABI" "$(get_soc_vendor)"
+            ;;
+        remove)
+            ;;
+    esac
+}
--- src/usr/lib/rsetup/cli/main.sh
+++ src/usr/lib/rsetup/cli/main.sh
@@ -9,6 +9,7 @@ source "/usr/lib/rsetup/cli/account.sh"
 source "/usr/lib/rsetup/cli/docker.sh"
 source "/usr/lib/rsetup/cli/u-boot-menu.sh"
 source "/usr/lib/rsetup/cli/wi-fi.sh"
+source "/usr/lib/rsetup/cli/kernel.sh"
 
 if $DEBUG
 then
