ea395389cb4ea0e725d250e077107b93ab2d3a88
ZHANG Yuntian <yt@radxa.com>
2023-03-11 18:39:56 +0800

    Allow product and SoC ID override

--- src/usr/lib/rsetup/mod/hwid.sh
+++ src/usr/lib/rsetup/mod/hwid.sh
@@ -1,9 +1,19 @@
 # shellcheck shell=bash
 
 get_soc_vendor() {
-    tr $"\0" $"\n" < /proc/device-tree/compatible | tail -n 1 | cut -d "," -f 1
+    if [[ -n "${RSETUP_SOC_VENDOR_OVERRIDE:=}" ]] || [[ ! -f /proc/device-tree/compatible ]]
+    then
+        echo "$RSETUP_SOC_VENDOR_OVERRIDE"
+    else
+        tr $"\0" $"\n" < /proc/device-tree/compatible | tail -n 1 | cut -d "," -f 1
+    fi
 }
 
 get_product_id() {
-    tr $"\0" $"\n" < /proc/device-tree/compatible | head -n 1 | cut -d "," -f 2
+    if [[ -n "${RSETUP_PRODUCT_ID_OVERRIDE:=}" ]] || [[ ! -f /proc/device-tree/compatible ]]
+    then
+        echo "$RSETUP_PRODUCT_ID_OVERRIDE"
+    else
+        tr $"\0" $"\n" < /proc/device-tree/compatible | head -n 1 | cut -d "," -f 2
+    fi
 }
