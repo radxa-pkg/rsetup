194d8b7a0647418e7538c668ed8be50cea1fed2c
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2023-03-18 17:05:01 +0800

    Fix parse_dtbo output format
    mapfile expect each array element to be separated by new line, not space.

--- src/usr/lib/rsetup/cli/u-boot-menu.sh
+++ src/usr/lib/rsetup/cli/u-boot-menu.sh
@@ -157,7 +157,7 @@ reset_overlays() {
 
 parse_dtbo() {
     local output
-    output="$(dtc -I dtb -O dts "$1" 2>/dev/null | dtc -I dts -O yaml 2>/dev/null | yq -r ".[0].metadata.$2[0]" | xargs -0)"
+    output="$(dtc -I dtb -O dts "$1" 2>/dev/null | dtc -I dts -O yaml 2>/dev/null | yq -r ".[0].metadata.$2[0]" | tr '\0' '\n')"
 
     if (( $# >= 3 ))
     then
