efe4579b91344a65bf7512152a4eed969beae9d6
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2023-06-15 15:00:40 +0800

    Fix overlay building issue

--- src/usr/lib/rsetup/mod/overlay.sh
+++ src/usr/lib/rsetup/mod/overlay.sh
@@ -6,7 +6,7 @@ compile_dtb() {
     # shellcheck disable=SC2064
     trap "rm -f $temp" RETURN EXIT
 
-    if ! cpp -x assembler-with-cpp -E -I "/usr/src/linux-headers-$(uname -r)/include" "$dts" "$temp"
+    if ! cpp -nostdinc -undef -x assembler-with-cpp -E -I "/usr/src/linux-headers-$(uname -r)/include" -I "/usr/lib/modules/$(uname -r)/build/include" "$dts" "$temp"
     then
         return 1
     fi
