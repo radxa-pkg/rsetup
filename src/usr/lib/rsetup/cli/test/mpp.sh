a143f336a81154fba7d92e267b43e4b1824ca200
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2023-09-20 20:09:37 +0800

    Update mpp debug functions

--- src/usr/lib/rsetup/cli/test/mpp.sh
+++ src/usr/lib/rsetup/cli/test/mpp.sh
@@ -3,11 +3,13 @@
 mpp_enable_logging() {
     export mpi_debug=1
     export mpp_debug=1
+    export mpp_dec_debug=1
     export h264d_debug=1
     export mpp_syslog_perror=1
     export mpp_log_level=6
-    export DISPLAY=":0"
     export GST_DEBUG="2,*mpp*:4,*fps*:7"
+    export LIBV4L_RKMPP_LOG_LEVEL=6
+    export DISPLAY=":0"
 }
 
 mpp_play() {
@@ -18,4 +20,9 @@ mpp_play() {
 mpp_gst_play() {
     mpp_enable_logging
     gst-play-1.0 "$1"
+}
+
+mpp_chromium() {
+    mpp_enable_logging
+    chromium --enable-logging --vmodule=/media/gpu=4 --v=1
 }
