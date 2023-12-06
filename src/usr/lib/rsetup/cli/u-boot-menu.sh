a4e3eb446cb5b5cea6bb2169ab1ae42f95419617
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2023-12-06 20:21:16 +0800

    Do not modify /etc/default/u-boot

--- src/usr/lib/rsetup/cli/u-boot-menu.sh
+++ src/usr/lib/rsetup/cli/u-boot-menu.sh
@@ -39,31 +39,6 @@ Please only enable one of them."
 }
 
 load_u-boot_setting() {
-    if [[ ! -e "/etc/default/u-boot" ]]
-    then
-        touch "/etc/default/u-boot"
-    fi
-
-    # shellcheck source=/dev/null
-    source "/etc/default/u-boot"
-
-    if [[ -z "${U_BOOT_TIMEOUT:-}" ]]
-    then
-        if ! grep -q "^U_BOOT_TIMEOUT" "/etc/default/u-boot"
-        then
-            echo 'U_BOOT_TIMEOUT="10"' >> "/etc/default/u-boot"
-        fi
-        sed -i "s/^U_BOOT_TIMEOUT=.*/U_BOOT_TIMEOUT=\"10\"/g" "/etc/default/u-boot"
-    fi
-    if [[ -z "${U_BOOT_PARAMETERS:-}" ]]
-    then
-        if ! grep -q "^U_BOOT_PARAMETERS" "/etc/default/u-boot"
-        then
-            echo "U_BOOT_PARAMETERS=\"\$(cat \"\/etc/kernel/cmdline\")\"" >> "/etc/default/u-boot"
-        fi
-        sed -i "s|^U_BOOT_PARAMETERS=.*|U_BOOT_PARAMETERS=\"\$(cat /etc/kernel/cmdline)\"|g" "/etc/default/u-boot"
-    fi
-
     # shellcheck source=/dev/null
     source "/etc/default/u-boot"
 
