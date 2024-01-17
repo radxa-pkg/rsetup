44cac6ce06d3cbd933626d2b4b38934a8d29520d
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2024-01-17 20:24:49 +0800

    feat: add uint_inputbox

--- src/usr/lib/rsetup/mod/dialog/modal_dialog.sh
+++ src/usr/lib/rsetup/mod/dialog/modal_dialog.sh
@@ -3,10 +3,10 @@
 # shellcheck disable=SC2120
 color_channel_picker() {
     local channel="$1" intensity
-    if ! intensity=$(inputbox "Please enter the intensity of the $channel channel:" "")
+    if ! intensity=$(uint_inputbox "Please enter the intensity of the $channel channel:" "")
     then
         return 1
-    elif ! (( 10#$intensity == 10#$intensity )) || (( 10#$intensity < 0 )) || (( 10#$intensity > 255))
+    elif (( 10#$intensity < 0 )) || (( 10#$intensity > 255))
     then
         msgbox "$channel channel value must be between 0 and 255. User entered '$intensity'."
         return 1
@@ -25,4 +25,17 @@ $r
 $g
 $b
 EOF
+}
+
+uint_inputbox() {
+    local item
+    if ! item=$(inputbox "$1" "$2")
+    then
+        return 1
+    elif ! (( 10#$item == 10#$item ))
+    then
+        msgbox "An unsigned integer is expected. User entered '$item'."
+        return 1
+    fi
+    echo "$item"
 }
