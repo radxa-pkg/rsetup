692c0a2c0a874413cbb87750bba56f172eb347fe
CodeChenL <2540735020@qq.com>
2024-02-26 14:43:51 +0800

    fix: fixes rsetup.service running connect_wi-fi error on startup

--- src/usr/lib/rsetup/cli/wi-fi.sh
+++ src/usr/lib/rsetup/cli/wi-fi.sh
@@ -3,14 +3,31 @@
 ALLOWED_RCONFIG_FUNC+=("connect_wi-fi")
 
 connect_wi-fi() {
+    local i ssid="$1" total_retry=10
+    local command=(nmcli device wifi connect "$ssid")
     case $# in
-        1)
+        1|2)
             nmcli radio wifi on
-            nmcli dev wifi connect "$1"
-            ;;
-        2)
-            nmcli radio wifi on
-            nmcli dev wifi connect "$1" password "$2"
+
+            if (( $# == 2 ))
+            then
+                command+=(password "$2")
+            fi
+
+            for ((i = 0; i < total_retry; i++))
+            do
+                if "${command[@]}"
+                then
+                    echo "Wi-Fi successfully connected to $ssid."
+                    return
+                else
+                    echo "Failed to connect to $ssid. Retry $((i + 1)) of $total_retry." >&2
+                    sleep 1
+                fi
+            done
+
+            echo "Wi-Fi failed to connect to $ssid after total of $total_retry retries. Quit." >&2
+            return 1
             ;;
         0|*)
             echo "Usage: ${FUNCNAME[0]} [ssid] <password>" >&2
