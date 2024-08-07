7bd358c97379951f5f229cc8c20ab8645f5fabe5
CodeChenL <2540735020@qq.com>
2024-08-07 16:33:38 +0800

    fix: task: change to use checklist

--- src/usr/lib/rsetup/tui/task/baota/baota.sh
+++ src/usr/lib/rsetup/tui/task/baota/baota.sh
@@ -1,31 +1,7 @@
 # shellcheck shell=bash
-
-__task_baota_uninstall() {
-    if yesno "Are you sure to uninstall Baota?"
-    then
-        if uninstall_baota
-        then
-            msgbox "Baota was successfully uninstalled."
-        else
-            msgbox "Failed to uninstall Baota." "$RTUI_PALETTE_ERROR"
-        fi
-    fi
-}
-
-__task_baota_install() {
-    if yesno "Are you sure you want to install Baota?"
-    then
-        install_baota
-    fi
-}
-
-__task_baota() {
-    menu_init
-    if [[ -f /www/server/panel/BT-Panel ]]
-    then
-        menu_add __task_baota_uninstall   "Uninstall Baota"
-    else
-        menu_add __task_baota_install     "Install Baota"
-    fi
-    menu_show "Please select an option below:"
-}
+if [[ -f /etc/init.d/bt ]]
+then
+    checklist_add "Uninstall Baota" "OFF" "uninstall_baota"
+else
+    checklist_add "Install Baota" "OFF" "install_baota"
+fi
