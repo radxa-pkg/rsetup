740bf4c165b5e74728a48f3c5d655c4ea654806a
Guoguo <i@qwq.trade>
2025-10-09 08:56:41 +0000

    feat: add edk2 support for dt overlays

---
+++ src/usr/lib/rsetup/cli/overlay-menu.sh
@@ -0,0 +1,58 @@
+# shellcheck shell=bash
+
+# shellcheck source=src/usr/lib/rsetup/cli/edk2-menu.sh
+source "/usr/lib/rsetup/cli/edk2-menu.sh"
+# shellcheck source=src/usr/lib/rsetup/cli/u-boot-menu.sh
+source "/usr/lib/rsetup/cli/u-boot-menu.sh"
+
+load_overlay_setting() {
+    if is_u-boot_exist; then
+        load_u-boot_setting
+    fi
+
+    if is_edk2_exist; then
+        load_edk2_setting
+    fi
+}
+
+update_overlay_entry() {
+    if is_u-boot_exist; then
+        u-boot-update
+    fi
+
+    if is_edk2_exist; then
+        update_entry_overlays
+    fi
+}
+
+disable_overlays() {
+    if is_u-boot_exist; then
+        disable_u-boot_overlays
+    fi
+
+    if is_edk2_exist; then
+        disable_edk2_overlays
+    fi
+}
+
+rebuild_overlays() {
+    if is_u-boot_exist; then
+        rebuild_u-boot_overlays "$@"
+    fi
+
+    if is_edk2_exist; then
+        rebuild_edk2_overlays "$@"
+    fi
+}
+
+enable_overlays() {
+    __parameter_count_at_least_check 1 "$@"
+
+    if is_u-boot_exist; then
+        enable_u-boot_overlays "$@"
+    fi
+
+    if is_edk2_exist; then
+        enable_edk2_overlays "$@"
+    fi
+}
--- src/usr/lib/rsetup/mod/overlay.sh
+++ src/usr/lib/rsetup/mod/overlay.sh
@@ -16,3 +16,179 @@ compile_dtb() {
         return 2
     fi
 }
+
+is_overlay_unchanged() {
+    [[ "$(__array_to_ordered_text "${RTUI_CHECKLIST_STATE_NEW[@]}")" == \
+       "$(__array_to_ordered_text "${RTUI_CHECKLIST_STATE_OLD[@]}")" ]]
+}
+
+check_overlay_conflict_init() {
+    RSETUP_OVERLAY_RESOURCES=()
+    RSETUP_OVERLAY_RESOURCE_OWNER=()
+}
+
+check_overlay_conflict() {
+    local name resources res i
+    mapfile -t resources < <(parse_dtbo "exclusive" "$1")
+    mapfile -t name < <(parse_dtbo --default-value "file" "title" "$1")
+
+    for res in "${resources[@]}"
+    do
+        if [[ "$res" == "null" ]]
+        then
+            continue
+        fi
+        if i="$(__in_array "$res" "${RSETUP_OVERLAY_RESOURCES[@]}")"
+        then
+            msgbox "Resource conflict detected!
+
+'${name[0]}' and '${RSETUP_OVERLAY_RESOURCE_OWNER[$i]}' both require the exclusive ownership of the following resource:
+
+${RSETUP_OVERLAY_RESOURCES[$i]}
+
+Please only enable one of them."
+            return 1
+        else
+            RSETUP_OVERLAY_RESOURCES+=("$res")
+            RSETUP_OVERLAY_RESOURCE_OWNER+=("${name[0]}")
+        fi
+    done
+}
+
+disable_overlays_general() {
+    for i in "$FDT_OVERLAYS_DIR"/*.dtbo
+    do
+        mv -- "$i" "${i}.disabled"
+    done
+}
+
+rebuild_overlays_general() {
+    local version="$1" vendor="${2:-}" dtbos i
+    local old_overlays new_overlays enabled_overlays=()
+
+    old_overlays="$(realpath "$FDT_OVERLAYS_DIR")"
+    new_overlays="${old_overlays}_new"
+
+    echo "Rebuilding overlay data folder for '$version'..."
+
+    if [[ -d "$old_overlays" ]]
+    then
+        cp -aR "$old_overlays" "$new_overlays"
+    else
+        mkdir -p "$old_overlays" "$new_overlays"
+    fi
+
+    if [[ -f "$new_overlays/managed.list" ]]
+    then
+        echo "Removing managed overlays..."
+        mapfile -t RSETUP_MANAGED_OVERLAYS < "$new_overlays/managed.list"
+
+        for i in "${RSETUP_MANAGED_OVERLAYS[@]}"
+        do
+            if [[ -f "$new_overlays/$i" ]]
+            then
+                enabled_overlays+=( "$i" )
+            fi
+            rm -f "$new_overlays/$i" "$new_overlays/$i.disabled"
+        done
+    fi
+
+    if [[ -n "$vendor" ]]
+    then
+        dtbos=( "/usr/lib/linux-image-$version/$vendor/overlays/"*.dtbo* )
+    else
+        dtbos=( "/usr/lib/linux-image-$version/"*"/overlays/"*.dtbo* )
+    fi
+    rm -f "$new_overlays/managed.list"
+    touch "$new_overlays/managed.list"
+
+    echo "Building list of compatible overlays..."
+    mapfile -t dtbos < <(dtbo_is_compatible "${dtbos[@]}")
+
+    # This loop is a bottleneck due to expensive operations
+    # Enabling parallelism brings total execution time from 38.453s to 21.633s
+    local nproc
+    nproc=$(( $(nproc) ))
+    echo "Installing compatible overlays..."
+    for i in "${dtbos[@]}"
+    do
+        while (( $(jobs -r | wc -l) > nproc ))
+        do
+            sleep 0.1
+        done
+        (
+            cp "$i" "$new_overlays/$(basename "$i").disabled"
+            exec 100>>"$new_overlays/managed.list"
+            flock 100
+            basename "$i" >&100
+        ) &
+    done
+    wait
+
+    # This loop is not a bottleneck
+    # We add parallelism to make it uniform
+    echo "Reenable previously enabled overlays..."
+    for i in "${enabled_overlays[@]}"
+    do
+        while (( $(jobs -r | wc -l) > nproc ))
+        do
+            sleep 0.1
+        done
+        (
+            if [[ -f "$new_overlays/${i}.disabled" ]]
+            then
+                mv -- "$new_overlays/${i}.disabled" "$new_overlays/$i"
+            fi
+        ) &
+    done
+    wait
+
+    echo "Commiting changes..."
+    rm -rf "${old_overlays}_old"
+    mv "$old_overlays" "${old_overlays}_old"
+    mv "$new_overlays" "$old_overlays"
+
+    echo "Overlay data folder has been successfully rebuilt."
+}
+
+enable_overlay_general() {
+    __parameter_count_at_least_check 1 "$@"
+
+    local enable_overlays_list i dir_name file_name input_check=true
+
+    for i in "$@"
+    do
+        dir_name="$(dirname "$i")"
+        file_name="$(basename "${i%.disabled}")"
+
+        if [[ "$dir_name" == "." ]]
+        then
+            dir_name="$FDT_OVERLAYS_DIR"
+        fi
+
+        if [[ "$(realpath "$dir_name")" != "$(realpath "$FDT_OVERLAYS_DIR")" ]]
+        then
+            echo "$i: only overlays within '$FDT_OVERLAYS_DIR' can be enabled." >&2
+            input_check=false
+        elif [[ -e "$dir_name/$file_name.disabled" ]]
+        then
+            enable_overlays_list+=("$FDT_OVERLAYS_DIR/$file_name")
+        elif [[ -e "$dir_name/$file_name" ]]
+        then
+            echo "$file_name: already enabled." >&2
+        else
+            echo "$file_name: cannot find such overlay in '$FDT_OVERLAYS_DIR'"
+            input_check=false
+        fi
+    done
+
+    if ! $input_check
+    then
+        return "$ERROR_ILLEGAL_PARAMETERS"
+    fi
+
+    for i in "${enable_overlays_list[@]}"
+    do
+        mv "$i.disabled" "$i"
+    done
+}
